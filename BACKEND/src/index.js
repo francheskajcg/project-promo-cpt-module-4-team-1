b1db6e0dacc00c14dd5303f96b4a36e2ee1bede1
Francheska Contreras Garcia <frankeka.jose@gmail.com>
2025-03-20 20:42:49 +0100

    first endpoint

--- BACKEND/src/index.js
+++ BACKEND/src/index.js
@@ -4,42 +4,45 @@ require("dotenv").config();
 const mysql = require("mysql2/promise");
 
 async function getConnection() {
-    const connectionData = {
-      host: process.env["MYSQL_HOST"],
-      port: process.env["MYSQL_PORT"],
-      user: process.env["MYSQL_USER"],
-      password: process.env["MYSQL_PASS"],
-      database: process.env["MYSQL_SCHEMA"],
-    };
-    const connection = await mysql.createConnection(connectionData);
-    await connection.connect();
-  
-    return connection;
-  }
-  
-  // Configuración del servidor
-  const app = express();
-  
-  app.use(cors());
-  app.use(express.json());
-  
-  // Arrancamos el servidor
-  const port = 3000;
-  app.listen(port, () => {
-    console.log(`Example app listening on port <http://localhost:${port}>`);
-  });
+  const connectionData = {
+    host: process.env["MYSQL_HOST"],
+    port: process.env["MYSQL_PORT"],
+    user: process.env["MYSQL_USER"],
+    password: process.env["MYSQL_PASS"],
+    database: process.env["MYSQL_SCHEMA"],
+  };
+  const connection = await mysql.createConnection(connectionData);
+  await connection.connect();
 
-  app.get("/api/autores", async (req, res) => {
-    const conn = await getConnection();
-  
-    const [results] = await conn.query(`SELECT * FROM coolproject.autores;`);
-  
-    await conn.end();
-  
-    const numOfElements = results.length;
-  
-    res.json({
-      info: { count: numOfElements }, 
-      results: results, 
-    });
+  return connection;
+}
+
+// Configuración del servidor
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Arrancamos el servidor
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port <http://localhost:${port}>`);
+});
+
+app.get("/api/autores", async (req, res) => {
+  const conn = await getConnection();
+
+  const [results] = await conn.query(`SELECT * 
+FROM coolproject.autores 
+ JOIN coolproject.proyectos 
+ON autores.id = proyectos.id;`);
+
+  await conn.end();
+
+  const numOfElements = results.length;
+
+  res.json({
+    info: { count: numOfElements },
+    results: results,
   });
+});
