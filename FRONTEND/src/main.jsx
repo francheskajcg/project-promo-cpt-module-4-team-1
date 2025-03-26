05a9a4f36fc014f5d550d0e0627464e5f823a02e
biancadragan <andreeabiancadragan@gmail.com>
2025-03-26 21:35:03 +0100

    Remove public frontend and change Browser por Hash

--- FRONTEND/src/main.jsx
+++ FRONTEND/src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App.jsx';
 
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-   
-  </StrictMode>,
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
 );
